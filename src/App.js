e97492f6deb16669371f99df5cb9c8e749088cae
Dustin Atwood <dustin@atwood.email>
2021-09-12 14:07:50 -0500

    package updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Url Shrink</h1>
+        <h1>Url Minify</h1>
         <Converter/>
       </header>
     </div>
